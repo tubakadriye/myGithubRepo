12cf66d8b720b7db8ba1b87b0aba84a2cbd8b714
Tuba Karaca <tubakaraca@Tubas-MacBook-Pro.local>
2023-06-04 16:53:07 +0200

    runnerClass

--- src/Calculator.java
+++ src/Calculator.java
@@ -11,4 +11,6 @@ public class Calculator {
     public int multiply(int a , int b) {
         return a*b;
     }
+
+
 }
---
+++ src/CalculatorRunner.java
@@ -0,0 +1,5 @@
+public class CalculatorRunner {
+    public static void main(String[] args) {
+
+    }
+}
